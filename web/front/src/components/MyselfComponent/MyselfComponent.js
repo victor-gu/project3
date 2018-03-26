7530b7b9101b595eea226955b5bd0dbd49061c4f
shushengge <438323593@qq.com>
2018-03-26 22:32:58 +0800

    8

---
+++ web/front/src/components/MyselfComponent/MyselfComponent.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react' 
+
+export default class MyslfComponent extends Component{
+    render(){
+        return (
+            <div>
+                <h1>好烦i阿姨父爱积分i啊</h1>
+            </div>
+        )
+    }
+}
--- web/front/src/router/router.js
+++ web/front/src/router/router.js
@@ -2,17 +2,17 @@ import React from 'react'
 import {Route} from 'react-router'
 
 import HomeComponent from '../components/home/HomeComponent'
-import SortComponent from '../components/SortComponent/SortComponent'
-import CartComponent from '../components/CartComponent/CartComponent'
+import SortComponent from '../components/sort/SortComponent'
+import CartComponent from '../components/cart/CartComponent'
 import MyselfComponent from '../components/MyselfComponent/MyselfComponent'
 
 
 export default (
     <Route>
         <Route path="/" component={HomeComponent} />
-        <Route path="/sort" component={HomeComponent} />
-        <Route path="/cart" component={HomeComponent} />
-        <Route path="/mine" component={HomeComponent} />
+        <Route path="/sort" component={SortComponent} />
+        <Route path="/cart" component={CartComponent} />
+        <Route path="/mine" component={MyselfComponent} />
     </Route>
   
 )
