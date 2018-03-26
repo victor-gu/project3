7ec2b615e1419bdc9206c0787996ddc1d5ed7966
shushengge <438323593@qq.com>
2018-03-26 22:58:32 +0800

    9

--- web/front/src/components/MyselfComponent/MyselfComponent.js
+++ web/front/src/components/MyselfComponent/MyselfComponent.js
@@ -1,10 +1,16 @@
 import React,{Component} from 'react' 
+import NavComponent from '../nav/NavComponent'
+import '../../common/common.css'
 
-export default class MyslfComponent extends Component{
+export default class MyselfComponent extends Component{
     render(){
         return (
-            <div>
-                <h1>好烦i阿姨父爱积分i啊</h1>
+            <div className="home">             
+               <div className="homeHeader"></div>
+               <div className="homeContent">
+                     <h1>梓峰的天下</h1>
+               </div>
+               <div className="homeFooter"><NavComponent /></div>
             </div>
         )
     }
--- web/front/src/components/cart/CartComponent.js
+++ web/front/src/components/cart/CartComponent.js
@@ -1,10 +1,16 @@
 import React,{Component} from 'react' 
+import NavComponent from '../nav/NavComponent'
+import '../../common/common.css'
 
 export default class CartComponent extends Component{
     render(){
         return (
-            <div>
-                <h1>好烦i阿姨父爱积分i啊</h1>
+            <div className="home">             
+               <div className="homeHeader"></div>
+               <div className="homeContent">
+                     <h1>兴胜爱的海洋</h1>
+               </div>
+               <div className="homeFooter"><NavComponent /></div>
             </div>
         )
     }
