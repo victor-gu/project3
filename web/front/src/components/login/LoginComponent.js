8dcfd5b14508fe3eae0753f3afede82482e1a06d
yangzulai <1141511321@qq.com>
2018-03-29 14:56:26 +0800

    login完善

--- web/front/src/components/login/LoginComponent.js
+++ web/front/src/components/login/LoginComponent.js
@@ -11,7 +11,7 @@ export default class SortComponent extends Component{
                     window.sessionStorage.setItem('xxtoken',res.data)
                     window.sessionStorage.setItem('userid',res.data1[0]._id)
                     window.sessionStorage.setItem('username',this.refs.name.value)
-                    //location.href ='/'
+                    location.href ='/'
                 }else{
                     alert("用户名或密码有误");
                     this.refs.name.value=null;
