007b56e182f1c6b1133f25043c2199b04bacde41
linzifeng12138 <2582605401@qq.com>
2018-03-29 15:03:15 +0800

    1、我的--页面获取已登录的username并显示

--- web/front/src/components/MyselfComponent/MyselfHeaderComponent/MyselfHeaderComponent.js
+++ web/front/src/components/MyselfComponent/MyselfHeaderComponent/MyselfHeaderComponent.js
@@ -7,20 +7,42 @@ import '../iconfont/lzffont/iconfont.css'
 import './lzfheader.css'
 
 export default class MyselfComponent extends React.Component{
-    componentWillMount(){
-        let userid = sessionStorage.getItem("userid");
-        console.log(userid);
-    }
+    // componentWillMount(){
+    //     let username = sessionStorage.getItem("username");
+    //     console.log(username);
+    // }
     render(){
+        let logincontent;
+        let username = sessionStorage.getItem("username");
+
+        let beforehtml = (
+            <div className="left_topnav">
+                <Link to ="login"><p className="lzf_loginbtn">
+                    登录 / 注册 
+                    </p></Link>
+                <h5 className="lzf_profit">登录更多优惠
+                </h5>
+            </div>
+        )
+
+        let afterhtml = (
+            <div className="left_topnav">
+                <p className="lzf_loginbtn">
+                    {username}
+                </p>
+            </div>
+        )
+        if(username){
+            logincontent = afterhtml
+        }else{
+            logincontent = beforehtml
+        }
+
         return(
             <div className = "lzf_topnavfixed" ref="lzf_header">
 
-                <div className="left_topnav">
-                    <Link to ="login"><p className="lzf_loginbtn">
-                        登录 / 注册 
-                        </p></Link>
-                    <h5 className="lzf_profit">登录更多优惠
-                    </h5>
+                <div>
+                    {logincontent}
                 </div>
 
                 <div className="right_topnav">
